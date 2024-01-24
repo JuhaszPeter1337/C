e90943923e2891165beb9075ad8307c8441e94a4
Peter Juhasz <juhasz.peter1337@gmail.com>
2024-01-24 23:48:18 +0100

    Added loops

---
+++ Loops/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char *name = "Hi, my name is Peter Juhasz!";
+
+	for (int i = 0; name[i] != '\0'; i++){
+		printf("%c", name[i]);
+	}
+
+    return 0;
+}
---
+++ Loops/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char *name = "Hi, my name is Peter Juhasz!";
+
+    int i = 0;
+
+	while (name[i] != '\0'){
+		printf("%c", name[i]);
+        i++;
+	}
+
+    return 0;
+}
