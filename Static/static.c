e47e02b0489329ec23f8e30d90babae7a1b0675b
Peter Juhasz <juhasz.peter1337@gmail.com>
2024-03-29 20:42:22 +0100

    Typo

--- Static/static.c
+++ Static/static.c
@@ -11,7 +11,7 @@ int main()
     /* The program prints 1 2 because static variables are only initialized once and live till the end of the program. 
        That is why they can retain their value between multiple function calls.
        A static int variable remains in memory while the program is running. 
-       A normal or auto variable is destroyed when a function call where the variable was declared is over.*/
+       A normal or auto variable is destroyed when a function call where the variable was declared is over. */
     printf("Number value: %d\n", increase_value());
     printf("Number value: %d\n", increase_value());
 
