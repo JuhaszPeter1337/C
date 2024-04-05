0f43cc70c1c1b15a843b30ba7875c04410059ea5
Peter Juhasz <juhasz.peter1337@gmail.com>
2024-04-05 21:39:19 +0200

    Split into files

---
+++ Deadline_diary/event.h
@@ -0,0 +1,32 @@
+#ifndef EVENT_H_INCLUDED
+#define EVENT_H_INCLUDED
+
+typedef struct Event{
+    char *name;
+    char *time;
+    char *location;
+    char *description;
+    struct Event *next;
+} Event;
+
+typedef Event* (*searchFunctionPtr)(Event*, char*);
+
+Event *add_event(Event *list, char *name, char *time, char *location, char *description);
+
+Event *delete_event(Event *list, char *name, char *date, char *location);
+
+Event *search_by_name(Event *list, char *name);
+Event *search_by_date(Event *list, char *date);
+Event *search_by_location(Event *list, char *location);
+
+int modify_event(Event *list, char *name, char *date, char *location, char *mit, char *mire);
+
+void print_events(Event *list);
+
+Event *print_list_by_year(Event *list, char *year);
+Event *print_list_by_month(Event *list, char *year, char *month);
+Event *print_list_by_day(Event *list, char *year, char *month, char *day);
+
+void freeList(Event *list);
+
+#endif // EVENT_H_INCLUDED
---
+++ Deadline_diary/options.h
@@ -0,0 +1,9 @@
+#ifndef OPTIONS_H_INCLUDED
+#define OPTIONS_H_INCLUDED
+
+void options();
+void options_for_searching();
+void options_for_filtering();
+void options_for_modifying();
+
+#endif // OPTIONS_H_INCLUDED
